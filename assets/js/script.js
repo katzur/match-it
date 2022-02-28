344efb9f71db8cfb5223f065852a52053e161d94
katzur <zurawska.kasia@gmail.com>
2022-02-28 20:07:37 +0000

    Add code formatting

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,13 +11,12 @@ let score = 1;
 function countDown() {
     timer.innerHTML = time;
     if (score < 10 && time > 0) {
-    ( time = time -1)
-    }
-    else if (time === 0) {       
-    // overlay text at the end of the game if time runs out
-    var popUp = document.getElementById('game-over-text');
-    popUp.classList.add('visible');
-    time = 0;
+        (time = time - 1)
+    } else if (time === 0) {
+        // overlay text at the end of the game if time runs out
+        var popUp = document.getElementById('game-over-text');
+        popUp.classList.add('visible');
+        time = 0;
     }
     // overlay text at the end of the game if player matches all the cards
     else {
@@ -38,8 +37,8 @@ function flipCard() {
     if (!hasFlippedCard) {
         // first time player clicks a card
         hasFlippedCard = true;
-        firstCard = this;   
-    }   else {
+        firstCard = this;
+    } else {
         // second time player clicks a card
         hasFlippedCard = false;
         secondCard = this;
@@ -56,14 +55,14 @@ function flipCard() {
 function checkForMatch() {
     if (firstCard.dataset.framework === secondCard.dataset.framework) {
         matchCards();
-// score increase
+        // score increase
         pairs.innerHTML = score;
         score = score + 1;
-    // victory pop up text at the end of the game
-    if (score === 10) {
-        var popUp = document.getElementById('victory-text');
-        popUp.classList.add('visible');
-    }
+        // victory pop up text at the end of the game
+        if (score === 10) {
+            var popUp = document.getElementById('victory-text');
+            popUp.classList.add('visible');
+        }
 
     } else {
         unflipCards();
@@ -71,34 +70,34 @@ function checkForMatch() {
 }
 
 //keeping cards face up after matching them
-function matchCards(){
+function matchCards() {
     firstCard.removeEventListener('click', flipCard);
     secondCard.removeEventListener('click', flipCard);
-    
+
     resetBoard();
 }
 
 //when cards don't match - flip back after a 1.1 second
-function unflipCards(){
+function unflipCards() {
     //locking the board to avoid game bug based on additional clicks before 1.1sec
     lockBoard = true;
 
     setTimeout(() => {
-    firstCard.classList.remove('flip');
-    secondCard.classList.remove('flip');
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
 
-    resetBoard();
+        resetBoard();
     }, 1100);
 }
 
 // after uncovering first two cards it allows the player to uncover next ones
-function resetBoard(){
+function resetBoard() {
     [hasFlippedCard, lockBoard] = [false, false];
     [firstCard, secondCard] = [null, null];
 }
 
 //shuffling the cards on load
-(function shuffle(){
+(function shuffle() {
     cards.forEach(card => {
         let randomPos = Math.floor(Math.random() * 18);
         card.style.order = randomPos;
